9970cc1ab7a745255e65d93b0ddb5ab6d9cb767d
James <zendarva@gmail.com>
2015-01-23 13:30:17 -0600

    Begining WSDL walker

--- src/main/java/com/suprtek/SoapMock/FileHandler.java
+++ src/main/java/com/suprtek/SoapMock/FileHandler.java
@@ -2,7 +2,16 @@ package com.suprtek.SoapMock;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.file.Files;
+
+import org.ow2.easywsdl.wsdl.WSDLFactory;
+import org.ow2.easywsdl.wsdl.api.Description;
+import org.ow2.easywsdl.wsdl.api.Service;
+import org.ow2.easywsdl.wsdl.api.WSDLException;
+import org.ow2.easywsdl.wsdl.api.WSDLReader;
 
 public class FileHandler {
 
@@ -12,6 +21,8 @@ public class FileHandler {
 	{
 		topLevelPath = TopLevelPath;
 		System.out.println("topLevelPath: " + topLevelPath);
+		
+		scanWSDLs(TopLevelPath);
 	}
 	
 	public String getWSDL(String path)
@@ -29,4 +40,55 @@ public class FileHandler {
 		}
 		return new String(returnMe);		
 	}	
+	
+	
+	private void scanWSDLs(String path)
+	{
+		File curDir = new File(path);
+		
+		if (!curDir.exists())
+		{
+			return;
+		}
+		
+		for ( File target : curDir.listFiles())
+		{
+			if (target.isDirectory())
+			{
+				scanWSDLs(target.getAbsolutePath());
+				continue;
+			}
+			
+			if (target.getName().endsWith(".wsdl"))
+			{
+				//there is a wsdl here, parse it for actions, and check to see if they exist.
+			}
+			continue;
+		}
+		
+	}
+	
+	private void readWSDL(File file)
+	{
+		try {
+			WSDLReader reader = WSDLFactory.newInstance().newWSDLReader();
+			Description desc = reader.read(new URL("File://"+file.getAbsolutePath()));
+			
+			Service serv = desc.getServices().get(0);
+			System.out.println("test");
+		} catch (WSDLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 }
