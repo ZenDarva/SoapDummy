0a9e324f4b96b1faafdc71d3a898665d7c141b09
BK Turley <kturley@suprtek.com>
2015-01-23 13:27:35 -0600

    added tests

--- src/main/java/com/suprtek/SoapMock/MockListenerTest.java
+++ src/main/java/com/suprtek/SoapMock/MockListenerTest.java
@@ -3,6 +3,7 @@ package com.suprtek.SoapMock;
 import static org.junit.Assert.*;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 
 import org.junit.Test;
@@ -10,13 +11,28 @@ import org.junit.Test;
 import fi.iki.elonen.NanoHTTPD;
 import fi.iki.elonen.NanoHTTPD.IHTTPSession;
 import fi.iki.elonen.NanoHTTPD.Response;
+import fi.iki.elonen.NanoHTTPD.Response.Status;
 import static org.mockito.Mockito.*
 ;public class MockListenerTest {
 
 	@Test
-	public void testMockListener() {
+	public void testMockListener_hasPortNegativeOneBeforeStarted() {
 		int testPortValue = 8080;
 		MockListener testObject = new MockListener(testPortValue);
+		int expectedValue = -1;
+		assertEquals(expectedValue, testObject.getListeningPort());
+	}
+	
+	@Test
+	public void testMockListener_hasPortSetAfterStarted() {
+		int testPortValue = 8080;
+		MockListener testObject = new MockListener(testPortValue);
+		try {
+			testObject.start();
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail();
+		}
 		assertEquals(testPortValue, testObject.getListeningPort());
 	}
 
@@ -29,8 +45,43 @@ import static org.mockito.Mockito.*
 		Response testResult = testObject.serve(mockihttpSession);
 		Response expectedResult = new NanoHTTPD.Response(":("); 
 		
-		assertSame(testResult.toString(), expectedResult.toString());
+		assertEquals(testResult.getClass(), expectedResult.getClass());
+		assertEquals(testResult.getMimeType(), expectedResult.getMimeType());
+		assertEquals(testResult.getRequestMethod(), expectedResult.getRequestMethod());
+		assertEquals(testResult.getStatus(), expectedResult.getStatus());
 		
 	}
-
+	
+	@Test
+	public void testServeIHTTPSession_WsdlRequests() {
+		int testPortValue = 8080;
+		MockListener testObject = new MockListener(testPortValue);
+		IHTTPSession mockihttpSession = mock(IHTTPSession.class);
+		when(mockihttpSession.getUri()).thenReturn("wsdl");
+		when(mockihttpSession.getQueryParameterString()).thenReturn("wsdl");
+		InputStream mockInputStream = mock(InputStream.class);
+		when(mockihttpSession.getInputStream()).thenReturn(mockInputStream);
+		try {
+			when(mockInputStream.available()).thenReturn(0);
+		} catch (IOException e) {
+			fail();
+		}
+		try {
+			when(mockInputStream.read(new byte[1000], 0, 999)).thenReturn(1234);
+		} catch (IOException e) {
+			fail();
+		}
+		
+		Response testResult = testObject.serve(mockihttpSession);
+		
+		FileHandler files = new FileHandler("C:\\temp");
+		Response expectedResult =  new NanoHTTPD.Response(Status.OK, "text/xml", files.getWSDL(mockihttpSession.getUri()));
+		
+		assertEquals(testResult.getClass(), expectedResult.getClass());
+		assertEquals(testResult.getMimeType(), expectedResult.getMimeType());
+		assertEquals(testResult.getRequestMethod(), expectedResult.getRequestMethod());
+		assertEquals(testResult.getStatus(), expectedResult.getStatus());
+		
+	}
+	
 }
